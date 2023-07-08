c04a7a293dc5caab7dcceb863cdc7c1fb3331842
Christopher Dirksen <chrisdirksen024@outlook.com>
2023-07-08 13:23:07 -0400

    problems with gitbash

--- script.js
+++ script.js
@@ -1,32 +1,67 @@
+const id = document.getElementById('id').value
+const category = document.getElementById('category').value
+const name = document.getElementById('name').value
+const note = document.getElementById('details').value
+const fs = require('fs')
 
-const id = document.getElementById('id')
 id.setAttribute('value', Date.now())
 console.log(id.value)
 
-document.getElementById('submit').addEventListener('click', function(e){
-    e.preventDefault()
+let fullData 
 
-    const id = document.getElementById('id').value
-    const category = document.getElementById('category').value
-    const name = document.getElementById('name').value
-    const note = document.getElementById('details').value
+fetch('./save')
+    .then(res => res.json())
+    .then(data => {
+        let fullData = data
 
-    var formData = {
-        id: id,
-        category: category,
-        name: name,
-        note: note
-    }
+        document.getElementById('submit').addEventListener('submit', function(e){
+            e.preventDefault()
 
-    var jsonData = JSON.stringify(formData)
+            var formData = {
+                id:id,
+                category:category,
+                name:name,
+                note:note
+            }
 
-    var fs = require('fs')
+            fullData.push(formData)
 
-    fs.appendFile('./save.json', jsonData, function (err){
-        if(err){
-            alert('Eror Saving File', err)
-        }else{
-            alert('file saved successfully')
-        }
+            const stringData = JSON.stringify(fullData)
+
+            fs.writeFile('./save.json', stringData, function(err){
+                if(err){
+                    alert('Error Saving File', err)
+                }else{
+                    alert('File Saved Successfully')
+                }
+            })
+        })
     })
-})
+
+// document.getElementById('submit').addEventListener('click', function(e){
+//     e.preventDefault()
+
+//     const id = document.getElementById('id').value
+//     const category = document.getElementById('category').value
+//     const name = document.getElementById('name').value
+//     const note = document.getElementById('details').value
+
+//     var formData = {
+//         id: id,
+//         category: category,
+//         name: name,
+//         note: note
+//     }
+
+//     var jsonData = JSON.stringify(formData)
+
+//     var fs = require('fs')
+
+//     fs.appendFile('./save.json', jsonData, function (err){
+//         if(err){
+//             alert('Eror Saving File', err)
+//         }else{
+//             alert('file saved successfully')
+//         }
+//     })
+// })
