a68fe70b8062235daa9fb24d95b6b5bb6092dd42
Christopher Dirksen <chrisdirksen024@outlook.com>
2023-07-12 14:06:41 -0400

    working on the fetch call

--- script.js
+++ script.js
@@ -2,60 +2,72 @@ const id = document.getElementById('id').value
 const category = document.getElementById('category').value
 const name = document.getElementById('name').value
 const note = document.getElementById('details').value
-const fs = require('fs')
+const submit = document.getElementById('submit')
+const form = document.getElementById('new-note')
+// const fs = require('fs')
 
-id.setAttribute('value', Date.now())
-console.log(id.value)
 
-let fullData
+document.getElementById('id').setAttribute('value', Date.now())
+console.log('id', id.value)
 
-const fetchNotes = async () => {
-        // .then(res => res.json())
-        // .then(data => {
-            const response = await fetch('./save.json');
-            const data = await response.json();
+// let fullData
+
+// const fetchNotes = async () => {
+
+//             const response = await fetch('./save.json')
+//             const data = await response.json()
             
-            fullData = data
+//             fullData = data
 
-            const noteList = document.createElement('div')
-            noteList.setAttribute('class', 'note')
+//             const noteList = document.createElement('div')
+//             noteList.setAttribute('class', 'note')
             
-            for(let i=0; i < data.length; ++i){
+//             for(let i=0; i < data.length; ++i){
 
-                const noteCategory = document.createElement('p')
-                noteCategory.innerHTML = data[i].category
+//                 const noteCategory = document.createElement('p')
+//                 noteCategory.innerHTML = data[i].category
                 
-                const noteName = document.createElement('p')
-                noteName.innerHTML = data[i].name
+//                 const noteName = document.createElement('p')
+//                 noteName.innerHTML = data[i].name
                 
-                const note = document.createElement('p')
-                note.innerHTML = data[i].note
+//                 const note = document.createElement('p')
+//                 note.innerHTML = data[i].note
 
-                noteList.appendChild(noteCategory)
-                noteList.appendChild(noteName)
-                noteList.appendChild(note)
+//                 noteList.appendChild(noteCategory)
+//                 noteList.appendChild(noteName)
+//                 noteList.appendChild(note)
 
-                alert(data[i])
+//                 alert(data[i])
                 
-            }
-}
+//             }
+// }
 
-window.addEventListener('load', function(){
-    fetchNotes()
-    // alert(fullData)
-})
+// form.addEventListener('submit', function(e){
+//     e.preventDefault()
 
-document.addEventListener('submit', function(e){
-    e.preventDefault()
+//     const formData = {
+//         id: id,
+//         category: category,
+//         name: name,
+//         note:note
+//     }
+
+//     fullData.push(formData)
+//     const jsonData = JSON.stringify(fullData)
     
-    const id = document.getElementById('id').value
-    const category = document.getElementById('category').value
-    const name = document.getElementById('name').value
-    const note = document.getElementById('note').value
+//     fs.appendFile('./save.json', jsonData, function (err){
+//                 if(err){
+//                     alert('Eror Saving File', err)
+//                 }else{
+//                     alert('file saved successfully')
+//                 }
+//     alert('fullData')
+// })})
 
-    console.log(fullData)
-})
-
+// window.addEventListener('load', function(){
+//     fetchNotes()
+//     alert(fullData)
+// })
 // document.getElementById('submit').addEventListener('click', function(e){
 //     e.preventDefault()
 
